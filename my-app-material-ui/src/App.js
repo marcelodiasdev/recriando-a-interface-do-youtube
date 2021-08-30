3a677b03393ddef0b690f955a659e8fad4ea9185
Marcelo Silva Dias <marcelo.dias@redspark.local>
2021-08-30 15:59:21 -0300

    add commit

---
+++ my-app-material-ui/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+      <Button variant="contained" color="primary"> Hello Material UI</Button>
+    
+  );
+}
+
+export default App;
